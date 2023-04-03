d4c6dc1d89f539ef71bb013f58d90686cefd18e7
ITFriends14 <128975094+ITFriends14@users.noreply.github.com>
2023-04-03 17:58:27 +0300

    Add files via upload

---
+++ урок 7/main.py
@@ -0,0 +1,137 @@
+#int float str turple list set dict
+
+#num = 10
+#while num:
+#    print(num)
+#   num -= 1
+#print(num)
+
+#for i in range(12,0,-2):
+#   print(i)
+
+#for i in range(55, 89):
+#   print(i)
+
+txt = "kjdslfggfdghdkg"
+#for i in txt:
+#    print(i)
+
+#for i in range(len(txt)):
+#    print(txt[i])
+
+#for i in range(len(txt)-1, 0, -1):
+#    print(txt[i])
+
+l = ['rom', 'vinetlobn', 'pop']
+for i in range(len(l)-1, -1, -1):
+    print(l[i])
+
+m = []
+
+m.append(txt)
+m.append(1)
+for i in m:
+    print(i)
+
+#num = 10
+#while(num):
+#    print(num)
+#    num -= 1
+
+#num = 10
+#while(10 - num):
+#    print(num)
+#    num -= 1
+
+#num = 11
+#n = 11
+#while(n - num):
+#    print(num)
+#    num -= 1
+
+#num = 11
+#n = num
+#while(n - num):
+#    print(num)
+#    num -= 1
+
+import turtle
+
+turtle.color('red')
+turtle.bgcolor("black")
+
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.clear()
+
+# for i in range(4):
+#     turtle.right(90)
+#     turtle.forward(100)
+
+# for i in range(10):
+#     turtle.right(15)
+#     turtle.forward(10)
+
+# for i in range(10):
+#     turtle.right(15)
+#     turtle.forward(10)
+#     turtle.right(90)
+#     turtle.forward(10)
+
+# for i in range(12):
+#     turtle.right(10)
+#     turtle.forward(10)
+
+#turtle.color(yellow)
+# for i in range(5):
+#     turtle.right(-17)
+#     turtle.forward(-20)
+
+turtle.left(180)
+turtle.forward(100)
+
+turtle.left(35)
+turtle.forward(100)
+
+turtle.left(-35)
+turtle.forward(100)
+
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(90)
+turtle.forward(100)
+
+turtle.left(90)
+for i in range(15):
+    turtle.right(-35)
+    turtle.forward(-15)
+
+turtle.left(105)
+turtle.forward(100)
+
+turtle.left(90)
+for i in range(15):
+    turtle.right(-35)
+    turtle.forward(-15)
+
+turtle.left(105)
+turtle.forward(100)
+
+turtle.left(90)
+turtle.forward(50)
+
+turtle.left(50)
+turtle.forward(150)
+
+
